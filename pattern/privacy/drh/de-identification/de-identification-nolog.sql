ca131d9b5d49872157ac0c9da368910088724b57
Anitha Varghese <anitha.varghese@citrusinformatics.com>
2024-07-31 17:43:59 +0530

    chore: move files folder

--- pattern/privacy/drh/de-identification/de-identification-nolog.sql
+++
@@ -1,8 +0,0 @@
-update uniform_resource_investigator 
-set email = anonymize_email(email);
-
-update uniform_resource_author 
-set email = anonymize_email(email);
-
-update uniform_resource_participant
-set age = generalize_age(CAST(age AS INTEGER));
