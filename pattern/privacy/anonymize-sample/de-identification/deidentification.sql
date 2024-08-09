406e1756479192aa80958e09c81cdb52e166c67c
Baasit <baasisek01@gmail.com>
2024-08-09 11:33:50 +0100

    refactor(app): orch canonical test case

--- pattern/privacy/anonymize-sample/de-identification/deidentification.sql
+++ pattern/privacy/anonymize-sample/de-identification/deidentification.sql
@@ -1,74 +1,35 @@
---TODO: Ask Bassit to create a unique value generator for ids 
---The function ulid() when invoked in any insert command in this file generated error 'No such function ulid()'.
---The file was executed using this command 'surveilr anonymize --sql deidentification.sql
-
---Example: modified the insert command like this 
---INSERT INTO orchestration_session_entry (orchestration_session_entry_id, session_id, ingest_src, ingest_table_name, elaboration)
---SELECT
-    --(ulid()) as orchestration_session_entry_id,
-    --orchestration_session_id,
-    --'de-identification',
-    --NULL,
-   -- '{"description": "Processing de-identification"}'
---FROM session_info;
---Save this file after changing as above .see the ulid function being called
---execute command 'surveilr anonymize --sql deidentification.sql'
---Error is generated
-
---TODO: Ask Basit to capture any errors,logs etc while executing the anonymize or any operation using surveilr
---example: if 'surveilr anonymize --sql deidentification.sql' caused any error,or any action(like number of records de-identified,tables that under went deidentification) must be logged in respective tables
---An issue has be created : Ensure Anonymize and Verification/Validation Operations Log to Orchestration Tables #166
-
--- Begin a transaction
-BEGIN;
-
 -- Perform De-identification
-UPDATE uniform_resource_investigator
-SET email = anonymize_email(email)
-WHERE email IS NOT NULL;
+UPDATE ur_ingest_session_imap_account
+SET
+    email = anonymize_email(email),
+    password = mask(password),
+    host = anonymize_name(host),
+    created_by = anonymize_name(created_by),
+    updated_by = anonymize_name(updated_by),
+    deleted_by = anonymize_name(deleted_by);
 
-UPDATE uniform_resource_author
-SET email = anonymize_email(email)
-WHERE email IS NOT NULL;
+UPDATE ur_ingest_session_imap_acct_folder
+SET
+    folder_name = anonymize_name(folder_name),
+    created_by = anonymize_name(created_by),
+    updated_by = anonymize_name(updated_by),
+    deleted_by = anonymize_name(deleted_by);
 
+UPDATE uniform_resource
+SET
+    uri = mask_phone(uri),
+    content_digest = hash(content_digest),
+    nature = anonymize_name(nature);
 
--- Insert into orchestration_nature if not exists
 INSERT OR IGNORE INTO orchestration_nature (orchestration_nature_id, nature)
 VALUES ('deidentify', 'De-identification');
 
--- Retrieve the device ID and orchestration nature ID
-WITH device_info AS (
-    SELECT device_id FROM device LIMIT 1
-),
-orch_nature_info AS (
-    SELECT orchestration_nature_id FROM orchestration_nature WHERE nature = 'De-identification' LIMIT 1
-)
-INSERT OR IGNORE INTO orchestration_session (orchestration_session_id, device_id, orchestration_nature_id, version, args_json, diagnostics_json, diagnostics_md)
-SELECT
-    'ORCSESSIONID-'||((SELECT COUNT(*) FROM orchestration_session) + 1) AS orchestration_session_id,    
-    d.device_id,
-    o.orchestration_nature_id,
-    '1.0',
-    '{"parameters": "de-identification"}',
-    '{"status": "started"}',
-    'Started de-identification process'
-FROM device_info d, orch_nature_info o;
+SELECT surveilr_device_id() AS device_id;
+SELECT surveilr_orchestration_nature_id('De-identification') AS orchestration_nature_id;
+SELECT surveilr_orchestration_context_session_id() AS orchestration_session_id;
 
--- Retrieve the new session ID
-WITH session_info AS (
-    SELECT orchestration_session_id FROM orchestration_session LIMIT 1
-)
-INSERT OR IGNORE INTO orchestration_session_entry (orchestration_session_entry_id, session_id, ingest_src, ingest_table_name, elaboration)
-SELECT
-    'ORCSESENID-'||((SELECT COUNT(*) FROM orchestration_session_entry) + 1) AS orchestration_session_entry_id,
-    orchestration_session_id,
-    'de-identification',
-    NULL,
-    '{"description": "Processing de-identification"}'
-FROM session_info;
 
--- Create a temporary table
-CREATE TEMP TABLE temp_session_info AS
+CREATE TEMPORARY TABLE temp_session_info AS
 SELECT
     orchestration_session_id,
     (SELECT orchestration_session_entry_id FROM orchestration_session_entry WHERE session_id = orchestration_session_id LIMIT 1) AS orchestration_session_entry_id
@@ -89,7 +50,7 @@ INSERT OR IGNORE INTO orchestration_session_exec (
     narrative_md
 )
 SELECT
-    'ORCHSESSEXID-' || ((SELECT COUNT(*) FROM orchestration_session_exec) + 1 ),
+    'ORCHSESSEXID-' || ((SELECT COUNT(*) FROM orchestration_session_exec) + 1),
     'De-identification',
     s.orchestration_session_id,
     s.orchestration_session_entry_id,
@@ -98,7 +59,7 @@ SELECT
     'email column in uniform_resource_investigator',
     'De-identification completed',
     CASE 
-        WHEN (SELECT changes() = 0) THEN 'No rows updated' 
+        WHEN (SELECT changes()) = 0 THEN 'No rows updated' 
         ELSE NULL 
     END,
     'username in email is masked'
@@ -118,7 +79,7 @@ INSERT OR IGNORE INTO orchestration_session_exec (
     narrative_md
 )
 SELECT
-    'ORCHSESSEXID-' || ((SELECT COUNT(*) FROM orchestration_session_exec) + 1) ,
+    'ORCHSESSEXID-' || ((SELECT COUNT(*) FROM orchestration_session_exec) + 1),
     'De-identification',
     s.orchestration_session_id,
     s.orchestration_session_entry_id,
@@ -127,37 +88,27 @@ SELECT
     'email column in uniform_resource_author',
     'De-identification completed',
     CASE 
-        WHEN (SELECT changes() = 0) THEN 'No rows updated' 
+        WHEN (SELECT changes()) = 0 THEN 'No rows updated' 
         ELSE NULL 
     END,
     'username in email is masked'
 FROM temp_session_info s;
 
--- Update orchestration_session to set finished timestamp and diagnostics
-UPDATE orchestration_session
-SET 
-    orch_finished_at = CURRENT_TIMESTAMP,
-    diagnostics_json = '{"status": "completed"}',
-    diagnostics_md = 'De-identification process completed'
-WHERE orchestration_session_id = (SELECT orchestration_session_id FROM temp_session_info LIMIT 1);
-
 -- Drop the temporary table when done
 DROP TABLE temp_session_info;
 
--- Commit the transaction
-COMMIT;
-
 -- Handle exceptions, errors if any in an external mechanism (not possible through SQLITE)
--- For example:
+-- Example:
 -- ROLLBACK;
--- INSERT INTO orchestration_session_exec (orchestration_session_exec_id, exec_nature, session_id, exec_code, exec_status, input_text, output_text, exec_error_text, narrative_md)
+-- INSERT INTO orchestration_session_exec 
+-- (orchestration_session_exec_id, exec_nature, session_id, exec_code, exec_status, input_text, output_text, exec_error_text, narrative_md)
 -- VALUES (
 --     'orc-exec-id-' || hex(randomblob(16)),
 --     'De-identification',
 --     (SELECT orchestration_session_id FROM orchestration_session LIMIT 1),
 --     'UPDATE commands executed',
 --     1,
---     'Data from uniform_resource_investigator, uniform_resource_author  tables',
+--     'Data from uniform_resource_investigator, uniform_resource_author tables',
 --     'Error occurred during de-identification',
 --     'Detailed error message here',
 --     'Error during update'
