072b5c53e0250babdba30a1e2699af3f38646fa0
Shahid N. Shah <shahid@shah.org>
2024-08-21 22:20:50 -0400

    refactor: use new SQLa reflect instead of SQLPageAide

--- prime/deps.ts
+++ prime/deps.ts
@@ -1,7 +1,8 @@
 // local: ../../../netspective-labs/sql-aide
-// remote: https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.5
+// remote: https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.7
 
 export * as path from "https://deno.land/std@0.224.0/path/mod.ts";
-export * as ws from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.5/lib/universal/whitespace.ts";
-export * as SQLa from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.5/render/mod.ts";
-export * as SQLPageAide from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.5/lib/sqlpage/mod.ts";
+export * as ws from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.7/lib/universal/whitespace.ts";
+export * as SQLa from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.7/render/mod.ts";
+export * as SQLa_typ from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.7/pattern/typical/mod.ts";
+export * as callable from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.7/lib/reflect/callable.ts";
