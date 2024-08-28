0e0205d21b690269c0e082da49b0836ea614382d
Anitha Varghese <anitha.varghese@citrusinformatics.com>
2024-08-28 19:27:06 +0530

    update: modify windows specific cmd

--- service/diabetes-research-hub/drhctl-test.ts
+++ service/diabetes-research-hub/drhctl-test.ts
@@ -3,7 +3,7 @@
 import { $ } from "https://deno.land/x/dax@0.4.0/mod.ts";
 import * as colors from "https://deno.land/std@0.224.0/fmt/colors.ts";
 
-// Define a helper function to fetch SQL content from a URL
+// Helper function to fetch SQL content from a URL
 async function fetchSqlContent(url: string): Promise<string> {
  const response = await fetch(url);
  if (!response.ok) {
@@ -12,7 +12,7 @@ async function fetchSqlContent(url: string): Promise<string> {
  return await response.text();
 }
 
-// Define a helper function to execute a command with SQL content
+// Helper function to execute a command with SQL content via STDIN
 async function executeCommandWithSqlSTDIN(sql: string, ...cmd: string[]) {
  console.log(colors.dim(`Running command: ${cmd.join(" ")}`));
 
@@ -33,6 +33,10 @@ async function executeCommandWithSqlSTDIN(sql: string, ...cmd: string[]) {
   Deno.exit(status.code);
  }
 }
+
+// Detect platform-specific command format
+const isWindows = Deno.build.os === "windows";
+const toolCmd = isWindows ? ".\\surveilr" : "./surveilr";
 
 // Base URL for the resource surveillance commons
 const RSC_BASE_URL =
@@ -68,15 +72,16 @@ if (Deno.args.length === 0) {
  Deno.exit(1);
 }
 
+console.log(`hi ${toolCmd}`);
+
 // Store the folder name in a variable
 const folderName = Deno.args[0];
 
 // Path to the SQLite database file
-const dbFilePath = "./resource-surveillance.sqlite.db";
+const dbFilePath = "resource-surveillance.sqlite.db";
 
 // Check and delete the file if it exists
 await checkAndDeleteFile(dbFilePath);
-const tool_md: string = ` ./surveilr`;
 
 // Log the start of the process
 console.log(colors.cyan(`Starting the process for folder: ${folderName}`));
@@ -84,12 +89,13 @@ console.log(colors.cyan(`Starting the process for folder: ${folderName}`));
 try {
  // Ingest files and orchestrate transform-csv
  console.log(colors.dim(`Ingesting files from folder: ${folderName}...`));
- const ingestCmd = ` ${tool_md} ingest files -r ${folderName}/`;
- const transformCmd = ` ${tool_md} orchestrate transform-csv`;
- console.log(colors.dim(`Executing command: ${ingestCmd}`));
- console.log(colors.dim(`Executing command: ${transformCmd}`));
- await $`${ingestCmd}`;
- await $`${transformCmd}`;
+ const ingesttransformCmd =
+  `${toolCmd} ingest files -r ${folderName}/ && ${toolCmd} orchestrate transform-csv`;
+ const transformCmd = `${toolCmd} orchestrate transform-csv`;
+ console.log(colors.dim(`Executing command: ${ingesttransformCmd}`));
+ //console.log(colors.dim(`Executing command: ${transformCmd}`));
+ await $`${ingesttransformCmd}`;
+ //await $`${transformCmd}`;
  console.log(
   colors.green("Files ingestion and CSV transformation successful."),
  );
@@ -101,7 +107,7 @@ try {
  console.log(colors.dim("Executing deidentification orchestration..."));
  await executeCommandWithSqlSTDIN(
   deidentificationSql,
-  "./surveilr",
+  toolCmd,
   "orchestrate",
   "-n",
   "deidentification",
@@ -116,7 +122,7 @@ try {
  );
  await executeCommandWithSqlSTDIN(
   vvSql,
-  "./surveilr",
+  toolCmd,
   "orchestrate",
   "-n",
   "v&v",
@@ -130,12 +136,12 @@ try {
  // Fetch and execute UX auto orchestration
  console.log("Executing UX auto orchestration...");
  const execUrl = `${RSC_BASE_URL}/ux.auto.sql`;
- await $`./surveilr orchestrate -n "v&v" -s ${execUrl}`;
+ await $`${toolCmd} orchestrate -n "v&v" -s ${execUrl}`;
  console.log(colors.green("UX auto orchestration completed successfully."));
 
  // Launch the SQLPage web UI
  console.log("SQLPage Web UI loading...");
- await $`./surveilr web-ui --port 9000`;
+ await $`${toolCmd} web-ui --port 9000`;
 } catch (error) {
  console.error(colors.red("An error occurred:"), error.message);
  Deno.exit(1);
