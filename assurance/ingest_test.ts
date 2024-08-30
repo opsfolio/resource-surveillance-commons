76c7f2bd5048d4c7e0473a275b5462208f193a22
Baasit <baasisek01@gmail.com>
2024-08-30 14:15:44 +0100

    test(app): added e2e test for orchestration

--- assurance/ingest_test.ts
+++ assurance/ingest_test.ts
@@ -1,8 +1,4 @@
-import {
-    assertAlmostEquals,
-    assertEquals,
-    assertExists,
-} from "jsr:@std/assert@1";
+import { assert, assertEquals, assertExists } from "jsr:@std/assert@1";
 import * as path from "https://deno.land/std@0.224.0/path/mod.ts";
 import { $ } from "https://deno.land/x/dax@0.39.2/mod.ts";
 import { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
@@ -16,84 +12,91 @@ import { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 // # 4. add multitenancy tests
 // // await $`surveilr orchestrate -n "dd" -s https://raw.githubusercontent.com/opsfolio/resource-surveillance-commons/main/pattern/privacy/anonymize-sample/de-identification/deidentification.sql -s ${REPO_DIR}/pattern/privacy/anonymize-sample/de-identification/`;
 
-export const E2E_TEST_DIR = path.join(Deno.cwd(), "assurance");
-export const ZIP_URL =
-    "https://synthetichealth.github.io/synthea-sample-data/downloads/10k_synthea_covid19_csv.zip";
-export const ZIP_FILE = path.join(E2E_TEST_DIR, "10k_synthea_covid19_csv.zip");
-export const INGEST_DIR = path.join(E2E_TEST_DIR, "ingest");
-export const DEFAULT_RSSD_PATH = path.join(
-    E2E_TEST_DIR,
-    "resource-surveillance.sqlite.db",
+const E2E_TEST_DIR = path.join(Deno.cwd(), "assurance");
+const ZIP_URL =
+  "https://synthetichealth.github.io/synthea-sample-data/downloads/10k_synthea_covid19_csv.zip";
+const ZIP_FILE = path.join(E2E_TEST_DIR, "10k_synthea_covid19_csv.zip");
+const INGEST_DIR = path.join(E2E_TEST_DIR, "ingest");
+const DEFAULT_RSSD_PATH = path.join(
+  E2E_TEST_DIR,
+  "resource-surveillance.sqlite.db",
 );
 const TEST_FIXTURES_DIR = path.join(E2E_TEST_DIR, "test-fixtures");
 
-async function countFilesInDirectory(directoryPath: string): Promise<number> {
-    let fileCount = 0;
+export async function countFilesInDirectory(
+  directoryPath: string,
+): Promise<number> {
+  let fileCount = 0;
 
-    for await (const dirEntry of Deno.readDir(directoryPath)) {
-        if (dirEntry.isFile) {
-            fileCount++;
-        }
+  for await (const dirEntry of Deno.readDir(directoryPath)) {
+    if (dirEntry.isFile) {
+      fileCount++;
     }
+  }
 
-    return fileCount;
+  return fileCount;
 }
 
 Deno.test("file ingestion", async (t) => {
-    await t.step({
-        name: "setup test environment",
-        fn: async () => {
-            if (!await Deno.stat(ZIP_FILE).catch(() => false)) {
-                await $`wget ${ZIP_URL}`;
-            }
+  await t.step({
+    name: "",
+    fn: async () => {
+      if (!await Deno.stat(ZIP_FILE).catch(() => false)) {
+        await $`wget ${ZIP_URL}`;
+      }
 
-            assertExists(
-                await Deno.stat(ZIP_FILE).catch(() => null),
-                "❌ Error: Data file does not exist after download.",
-            );
+      assertExists(
+        await Deno.stat(ZIP_FILE).catch(() => null),
+        "❌ Error: Data file does not exist after download.",
+      );
 
-            if (!await Deno.stat(INGEST_DIR).catch(() => false)) {
-                await Deno.mkdir(INGEST_DIR, { recursive: true });
-                const unzipResult = await $`unzip ${ZIP_FILE} -d ${INGEST_DIR}`;
+      if (!await Deno.stat(INGEST_DIR).catch(() => false)) {
+        await Deno.mkdir(INGEST_DIR, { recursive: true });
+        const unzipResult = await $`unzip ${ZIP_FILE} -d ${INGEST_DIR}`;
 
-                assertEquals(
-                    unzipResult.code,
-                    0,
-                    "❌ Error: Failed to unzip the data file.",
-                );
-            }
-
-            assertExists(
-                await Deno.stat(INGEST_DIR).catch(() => null),
-                `❌ Error: Ingest directory ${INGEST_DIR} does not exist after unzipping.`,
-            );
-        },
-        ignore: false,
-    });
-
-    await t.step("surveilr ingest files", async () => {
-        assertExists(
-            await Deno.stat(INGEST_DIR).catch(() => null),
-            `❌ Error: Ingest directory ${INGEST_DIR} before surveilr ingest`,
-        );
-        const ingestResult =
-            await $`surveilr ingest files -d ${DEFAULT_RSSD_PATH} -r ${INGEST_DIR}/10k_synthea_covid19_csv -r ${TEST_FIXTURES_DIR}`;
         assertEquals(
-            ingestResult.code,
-            0,
-            `❌ Error: Failed to ingest data in ${INGEST_DIR}/10k_synthea_covid19_csv`,
+          unzipResult.code,
+          0,
+          "❌ Error: Failed to unzip the data file.",
         );
+      }
 
-        assertExists(
-            await Deno.stat(DEFAULT_RSSD_PATH).catch(() => null),
-            `❌ Error: ${DEFAULT_RSSD_PATH} does not exist`,
-        );
-    });
+      assertExists(
+        await Deno.stat(INGEST_DIR).catch(() => null),
+        `❌ Error: Ingest directory ${INGEST_DIR} does not exist after unzipping.`,
+      );
+    },
+    ignore: false,
+  });
 
-    const db = new DB(DEFAULT_RSSD_PATH);
+  await t.step("surveilr ingest files", async () => {
+    assertExists(
+      await Deno.stat(INGEST_DIR).catch(() => null),
+      `❌ Error: Ingest directory ${INGEST_DIR} before surveilr ingest`,
+    );
 
-    await t.step("file change history", async () => {
-        db.execute(`
+    if (await Deno.stat(DEFAULT_RSSD_PATH).catch(() => null)) {
+      await Deno.remove(DEFAULT_RSSD_PATH).catch(() => false);
+    }
+
+    const ingestResult =
+      await $`surveilr ingest files -d ${DEFAULT_RSSD_PATH} -r ${INGEST_DIR}/10k_synthea_covid19_csv -r ${TEST_FIXTURES_DIR}`;
+    assertEquals(
+      ingestResult.code,
+      0,
+      `❌ Error: Failed to ingest data in ${INGEST_DIR}/10k_synthea_covid19_csv`,
+    );
+
+    assertExists(
+      await Deno.stat(DEFAULT_RSSD_PATH).catch(() => null),
+      `❌ Error: ${DEFAULT_RSSD_PATH} does not exist`,
+    );
+  });
+
+  const db = new DB(DEFAULT_RSSD_PATH);
+
+  await t.step("file change history", async () => {
+    db.execute(`
             DROP VIEW IF EXISTS file_change_history;
             CREATE VIEW file_change_history AS
             SELECT
@@ -110,21 +113,21 @@ Deno.test("file ingestion", async (t) => {
             GROUP BY ur.uniform_resource_id, ur.uri;
         `);
 
-        const result = db.query(
-            `SELECT COUNT(*) AS count FROM file_change_history`,
-        );
-        assertEquals(result.length, 1);
+    const result = db.query(
+      `SELECT COUNT(*) AS count FROM file_change_history`,
+    );
+    assertEquals(result.length, 1);
 
-        const filesInDir = await countFilesInDirectory(
-            `${INGEST_DIR}/10k_synthea_covid19_csv`,
-        ) + (await countFilesInDirectory(TEST_FIXTURES_DIR));
+    const filesInDir = await countFilesInDirectory(
+      `${INGEST_DIR}/10k_synthea_covid19_csv`,
+    ) + (await countFilesInDirectory(TEST_FIXTURES_DIR));
 
-        const files = Number(result[0][0]);
-        assertAlmostEquals(files, filesInDir, 10);
-    });
+    const files = Number(result[0][0]);
+    assert(filesInDir > files);
+  });
 
-    await t.step("user activity summary", () => {
-        db.execute(`
+  await t.step("user activity summary", () => {
+    db.execute(`
             DROP VIEW IF EXISTS user_activity_summary;
             CREATE VIEW user_activity_summary AS
             SELECT
@@ -140,75 +143,75 @@ Deno.test("file ingestion", async (t) => {
             GROUP BY p.person_id, full_name;
         `);
 
-        const result = db.query(
-            `SELECT COUNT(*) AS count FROM user_activity_summary`,
-        );
-        assertEquals(result.length, 1);
-        assertEquals(result[0][0], 0);
-    });
+    const result = db.query(
+      `SELECT COUNT(*) AS count FROM user_activity_summary`,
+    );
+    assertEquals(result.length, 1);
+    assertEquals(result[0][0], 0);
+  });
 
-    await t.step("automatically transformed resources", () => {
-        const result = db.query(
-            `SELECT COUNT(*) AS row_count FROM uniform_resource_transform;`,
-        );
-        assertEquals(result.length, 1);
-        assertEquals(result[0][0], 3);
-    });
+  await t.step("automatically transformed resources", () => {
+    const result = db.query(
+      `SELECT COUNT(*) AS row_count FROM uniform_resource_transform;`,
+    );
+    assertEquals(result.length, 1);
+    assertEquals(result[0][0], 3);
+  });
 
-    // await t.step("compliance violations", () => {
-    //     db.execute(`
-    //         DROP VIEW IF EXISTS compliance_violations;
-    //         CREATE VIEW compliance_violations AS
-    //         SELECT
-    //             ur.uniform_resource_id,
-    //             ur.uri,
-    //             isfpe.file_path_abs,
-    //             CASE
-    //                 WHEN ur.content LIKE '%confidential%' OR ur.content LIKE '%secret%' THEN 'Sensitive Data Exposure'
-    //                 WHEN ur.content LIKE '%password%' OR ur.content LIKE '%credit card%' THEN 'PII Exposure'
-    //                 ELSE 'Other Violation'
-    //             END AS violation_type
-    //         FROM uniform_resource ur
-    //         JOIN ur_ingest_session_fs_path_entry isfpe ON ur.uniform_resource_id = isfpe.uniform_resource_id
-    //         WHERE ur.content LIKE '%confidential%' OR ur.content LIKE '%secret%'
-    //         OR ur.content LIKE '%password%' OR ur.content LIKE '%credit card%';
-    //     `);
+  // await t.step("compliance violations", () => {
+  //     db.execute(`
+  //         DROP VIEW IF EXISTS compliance_violations;
+  //         CREATE VIEW compliance_violations AS
+  //         SELECT
+  //             ur.uniform_resource_id,
+  //             ur.uri,
+  //             isfpe.file_path_abs,
+  //             CASE
+  //                 WHEN ur.content LIKE '%confidential%' OR ur.content LIKE '%secret%' THEN 'Sensitive Data Exposure'
+  //                 WHEN ur.content LIKE '%password%' OR ur.content LIKE '%credit card%' THEN 'PII Exposure'
+  //                 ELSE 'Other Violation'
+  //             END AS violation_type
+  //         FROM uniform_resource ur
+  //         JOIN ur_ingest_session_fs_path_entry isfpe ON ur.uniform_resource_id = isfpe.uniform_resource_id
+  //         WHERE ur.content LIKE '%confidential%' OR ur.content LIKE '%secret%'
+  //         OR ur.content LIKE '%password%' OR ur.content LIKE '%credit card%';
+  //     `);
 
-    //     const result = db.query(
-    //         `SELECT COUNT(*) AS count FROM compliance_violations`,
-    //     );
-    //     console.log({ result });
-    //     assertEquals(result.length, 1);
-    //     assertEquals(result[0][0], 0);
-    // });
+  //     const result = db.query(
+  //         `SELECT COUNT(*) AS count FROM compliance_violations`,
+  //     );
+  //     console.log({ result });
+  //     assertEquals(result.length, 1);
+  //     assertEquals(result[0][0], 0);
+  // });
 
-    // await t.step("potentially risky files", () => {
-    //     db.execute(`
-    //        DROP VIEW IF EXISTS potential_risk_files;
-    //         CREATE VIEW potential_risk_files AS
-    //         SELECT
-    //             ur.uniform_resource_id,
-    //             ur.uri,
-    //             ur.content_digest,
-    //             CASE
-    //                 WHEN ur.nature LIKE '%executable%' THEN 'Executable'
-    //                 WHEN ur.nature LIKE '%script%' THEN 'Script'
-    //                 WHEN isfpe.file_extn IN ('.exe', '.dll', '.bat', '.ps1', '.sh') THEN 'Suspicious Extension'
-    //                 ELSE 'Other'
-    //             END AS risk_category
-    //         FROM uniform_resource ur
-    //         JOIN ur_ingest_session_fs_path_entry isfpe ON ur.uniform_resource_id = isfpe.uniform_resource_id
-    //         WHERE ur.nature LIKE '%executable%' OR ur.nature LIKE '%script%'
-    //         OR isfpe.file_extn IN ('.exe', '.dll', '.bat', '.ps1', '.sh');
-    //     `);
+  // await t.step("potentially risky files", () => {
+  //     db.execute(`
+  //        DROP VIEW IF EXISTS potential_risk_files;
+  //         CREATE VIEW potential_risk_files AS
+  //         SELECT
+  //             ur.uniform_resource_id,
+  //             ur.uri,
+  //             ur.content_digest,
+  //             CASE
+  //                 WHEN ur.nature LIKE '%executable%' THEN 'Executable'
+  //                 WHEN ur.nature LIKE '%script%' THEN 'Script'
+  //                 WHEN isfpe.file_extn IN ('.exe', '.dll', '.bat', '.ps1', '.sh') THEN 'Suspicious Extension'
+  //                 ELSE 'Other'
+  //             END AS risk_category
+  //         FROM uniform_resource ur
+  //         JOIN ur_ingest_session_fs_path_entry isfpe ON ur.uniform_resource_id = isfpe.uniform_resource_id
+  //         WHERE ur.nature LIKE '%executable%' OR ur.nature LIKE '%script%'
+  //         OR isfpe.file_extn IN ('.exe', '.dll', '.bat', '.ps1', '.sh');
+  //     `);
 
-    //     const result = db.query(
-    //         `SELECT COUNT(*) AS count FROM potential_risk_files`,
-    //     );
-    //     console.log({ result });
-    //     assertEquals(result.length, 1);
-    //     // assertEquals(result[0][0], 0);
-    // });
+  //     const result = db.query(
+  //         `SELECT COUNT(*) AS count FROM potential_risk_files`,
+  //     );
+  //     console.log({ result });
+  //     assertEquals(result.length, 1);
+  //     // assertEquals(result[0][0], 0);
+  // });
 
-    db.close();
+  db.close();
 });
---
+++ assurance/orchestration_test.ts
@@ -0,0 +1,245 @@
+import { join, parse } from "https://deno.land/std@0.224.0/path/mod.ts";
+import {
+  assert,
+  assertEquals,
+  assertExists,
+  assertNotEquals,
+} from "jsr:@std/assert@1";
+import { $ } from "https://deno.land/x/dax@0.39.2/mod.ts";
+import { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
+import { countFilesInDirectory } from "./ingest_test.ts";
+
+const E2E_TEST_DIR = join(Deno.cwd(), "assurance");
+const DRH_DIR = join(Deno.cwd(), "service/diabetes-research-hub");
+const STUDY_FILES_ZIP = join(DRH_DIR, "study-files.zip");
+const STUDY_FILES_INGEST_DIR = join(DRH_DIR, "study-files");
+const RSSD_PATH = join(
+  E2E_TEST_DIR,
+  "orchestration-e2e-test.sqlite.db",
+);
+
+async function listFilesWithoutExtension(directory: string) {
+  const filesWithoutExtension: string[] = [];
+
+  for await (const dirEntry of Deno.readDir(directory)) {
+    if (dirEntry.isFile) {
+      const { name } = parse(dirEntry.name);
+      filesWithoutExtension.push(name);
+    }
+  }
+  return filesWithoutExtension;
+}
+
+function toSqlFriendlyIdentifier(input: string): string {
+  const re = /[^a-zA-Z0-9_]+/g;
+
+  let result = input.replace(re, "_").toLowerCase();
+
+  if (result.length > 0 && result[0].match(/[0-9]/)) {
+    result = "_" + result;
+  }
+
+  return result;
+}
+
+Deno.test("orchestration and transformations", async (t) => {
+  assertExists(
+    await Deno.stat(STUDY_FILES_ZIP).catch(() => null),
+    "❌ Error: Study Files Zip does not exist",
+  );
+
+  if (!await Deno.stat(STUDY_FILES_INGEST_DIR).catch(() => false)) {
+    await Deno.mkdir(STUDY_FILES_INGEST_DIR, { recursive: true });
+    const unzipResult =
+      await $`unzip ${STUDY_FILES_ZIP} -d ${STUDY_FILES_INGEST_DIR}`;
+
+    assertEquals(
+      unzipResult.code,
+      0,
+      "❌ Error: Failed to unzip the data file.",
+    );
+  }
+
+  assertExists(
+    await Deno.stat(STUDY_FILES_INGEST_DIR).catch(() => null),
+    `❌ Error: Ingest directory ${STUDY_FILES_INGEST_DIR} does not exist after unzipping.`,
+  );
+
+  await t.step("ingest study files", async () => {
+    assertExists(
+      await Deno.stat(STUDY_FILES_INGEST_DIR).catch(() => null),
+      `❌ Error: Ingest directory ${STUDY_FILES_INGEST_DIR} does not exist before surveilr ingest`,
+    );
+
+    if (await Deno.stat(RSSD_PATH).catch(() => null)) {
+      await Deno.remove(RSSD_PATH).catch(() => false);
+    }
+
+    const ingestResult =
+      await $`surveilr ingest files -d ${RSSD_PATH} -r ${STUDY_FILES_INGEST_DIR}/study-files`;
+    assertEquals(
+      ingestResult.code,
+      0,
+      `❌ Error: Failed to ingest data in ${STUDY_FILES_INGEST_DIR}/study-files`,
+    );
+
+    assertExists(
+      await Deno.stat(RSSD_PATH).catch(() => null),
+      `❌ Error: ${RSSD_PATH} does not exist`,
+    );
+  });
+
+  const db = new DB(RSSD_PATH);
+
+  await t.step("verify resources", async () => {
+    const csvUrs = db.query(
+      `SELECT COUNT(*) AS count FROM uniform_resource`,
+    );
+
+    const study_files_count = await countFilesInDirectory(
+      `${STUDY_FILES_INGEST_DIR}/study-files`,
+    );
+    assertEquals(csvUrs.length, 1);
+    assertEquals(csvUrs[0][0], study_files_count);
+  });
+
+  await t.step("transform csvs", async () => {
+    const transformResult =
+      await $`surveilr orchestrate -d ${RSSD_PATH} transform-csv`;
+    assertEquals(
+      transformResult.code,
+      0,
+      `❌ Error: Failed to transform CSVs in ${RSSD_PATH}`,
+    );
+  });
+
+  await t.step("verify transformed csvs", async () => {
+    const csvFileNames = await listFilesWithoutExtension(
+      `${STUDY_FILES_INGEST_DIR}/study-files`,
+    );
+    for (let fileName of csvFileNames) {
+      fileName = toSqlFriendlyIdentifier(fileName);
+      fileName = `uniform_resource_${fileName}`;
+      const query =
+        `SELECT name FROM sqlite_master WHERE type='table' AND name=?`;
+      const result = db.query(query, [fileName]);
+      assert(
+        result.length > 0,
+        `${fileName} does not exist as a transformed table in ${RSSD_PATH}`,
+      );
+    }
+  });
+
+  const urAuthorEmailsBeforeDeidentification = db.query(
+    "SELECT email FROM uniform_resource_author",
+  );
+  const urInvestigatorEmailsBeforeDeidentification = db.query(
+    "SELECT email FROM uniform_resource_investigator",
+  );
+
+  await t.step("execute deidentification script through stdin", async () => {
+    const sciptPath = `${DRH_DIR}/de-identification/drh-deidentification.sql`;
+    const orchestrateResult =
+      await $`cat ${sciptPath} | surveilr orchestrate -d ${RSSD_PATH} -n "dd"`;
+    assertEquals(
+      orchestrateResult.code,
+      0,
+      `❌ Error: Failed to execute ${sciptPath} against ${RSSD_PATH}`,
+    );
+  });
+
+  await t.step("verify orchestrated deidentification script", () => {
+    const orchestrationNatureId = db.query(
+      "SELECT orchestration_nature_id FROM orchestration_nature WHERE nature = 'De-identification'",
+    );
+    assertEquals(orchestrationNatureId.length, 1);
+    assertEquals(orchestrationNatureId[0][0], "deidentification");
+
+    const urAuthorEmailsAfterDeidentification = db.query(
+      "SELECT email FROM uniform_resource_author",
+    );
+    const urInvestigatorEmailsAfterDeidentification = db.query(
+      "SELECT email FROM uniform_resource_investigator",
+    );
+
+    assertNotEquals(
+      urAuthorEmailsAfterDeidentification,
+      urAuthorEmailsBeforeDeidentification,
+      "❌ Error: uniform_resource_author emails were not deidentified.",
+    );
+    assertNotEquals(
+      urInvestigatorEmailsBeforeDeidentification,
+      urInvestigatorEmailsAfterDeidentification,
+      "❌ Error: uniform_resource_author emails were not deidentified.",
+    );
+  });
+
+  await t.step("execute remote v&v script", async () => {
+    const sciptUrl =
+      `https://raw.githubusercontent.com/opsfolio/resource-surveillance-commons/main/service/diabetes-research-hub/verfication-validation/orchestrate-drh-vv.sql`;
+    const orchestrateResult =
+      await $`surveilr orchestrate -d ${RSSD_PATH} -n "v&v" -s ${sciptUrl}`;
+    assertEquals(
+      orchestrateResult.code,
+      0,
+      `❌ Error: Failed to execute ${sciptUrl} against ${RSSD_PATH}`,
+    );
+  });
+
+  await t.step("verify orchestrated remote v&v script", async (t) => {
+    await t.step("verify orchestration nature ID", () => {
+      const orchestrationNatureId = db.query(
+        "SELECT orchestration_nature_id FROM orchestration_nature WHERE nature = 'Verification and Validation'",
+      );
+      assertEquals(
+        orchestrationNatureId.length,
+        1,
+        "❌ Error: Incorrect number of entries for 'Verification and Validation' nature.",
+      );
+      assertEquals(
+        orchestrationNatureId[0][0],
+        "V&V",
+        "❌ Error: Orchestration nature ID is not 'V&V'.",
+      );
+    });
+
+    await t.step("verify missing columns logging", () => {
+      const missingColumns = db.query(
+        `SELECT COUNT(*) FROM orchestration_session_issue
+                WHERE issue_type = 'Schema Validation: Missing Columns'
+                AND session_id = (SELECT orchestration_session_id FROM orchestration_session WHERE orchestration_nature_id = 'V&V')`,
+      );
+      assert(
+        Number(missingColumns[0][0]) >= 0,
+        "❌ Error: Missing columns were not logged correctly in orchestration_session_issue.",
+      );
+    });
+
+    await t.step("verify additional columns logging", () => {
+      const additionalColumns = db.query(
+        `SELECT COUNT(*) FROM orchestration_session_issue
+                WHERE issue_type = 'Schema Validation: Additional Columns'
+                AND session_id = (SELECT orchestration_session_id FROM orchestration_session WHERE orchestration_nature_id = 'V&V')`,
+      );
+      assert(
+        Number(additionalColumns[0][0]) >= 0,
+        "❌ Error: Additional columns were not logged correctly in orchestration_session_issue.",
+      );
+    });
+
+    await t.step("verify data integrity checks logging", () => {
+      const dataIntegrityIssues = db.query(
+        `SELECT COUNT(*) FROM orchestration_session_issue
+                WHERE issue_type IN ('Data Integrity Checks: Invalid Dates', 'Data Integrity Checks: Invalid Age Values', 'Data Integrity Checks: Empty Cells')
+                AND session_id = (SELECT orchestration_session_id FROM orchestration_session WHERE orchestration_nature_id = 'V&V')`,
+      );
+      assert(
+        Number(dataIntegrityIssues[0][0]) >= 0,
+        "❌ Error: Data integrity issues were not logged correctly in orchestration_session_issue.",
+      );
+    });
+  });
+
+  //TODO: notebook orchestration
+  db.close();
+});
