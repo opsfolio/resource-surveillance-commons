03f89d78f34a4b96d7816507b657730cdf496f2c
Anitha Varghese <anitha.varghese@citrusinformatics.com>
2024-09-02 13:07:20 +0530

    add: drhctl test

---
+++ service/diabetes-research-hub/drhctl-test.ts
@@ -0,0 +1,184 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run
+
+import * as colors from "https://deno.land/std@0.224.0/fmt/colors.ts";
+import * as drhux from "./ux.sql.ts";
+import {
+ FlexibleTextSupplierSync,
+ spawnedResult,
+ textFromSupplierSync,
+} from "../../lib/universal/spawn.ts";
+
+// Detect platform-specific command format
+const isWindows = Deno.build.os === "windows";
+const toolCmd = isWindows ? ".\\surveilr" : "./surveilr";
+
+const RSC_BASE_URL =
+ "https://raw.githubusercontent.com/opsfolio/resource-surveillance-commons/main/service/diabetes-research-hub";
+
+// Helper function to fetch SQL content
+async function fetchSqlContent(url: string): Promise<string> {
+ try {
+  const response = await fetch(url);
+  if (!response.ok) {
+   throw new Error(`Failed to fetch SQL content from ${url}`);
+  }
+  return response.text();
+ } catch (error) {
+  console.error(
+   colors.red(`Error fetching SQL content from ${url}:`),
+   error.message,
+  );
+  Deno.exit(1);
+ }
+}
+
+// Helper function to execute a command
+async function executeCommand(
+ cmd: string[],
+ stdinSupplier?: FlexibleTextSupplierSync,
+) {
+ try {
+  console.log(colors.dim(`Executing command: ${cmd.join(" ")}`));
+
+  // Use spawnedResult for executing the command
+  const result = await spawnedResult(
+   cmd,
+   undefined,
+   stdinSupplier ? textFromSupplierSync(stdinSupplier) : undefined,
+  );
+
+  if (!result.success) {
+   throw new Error(`Command failed with status ${result.code}`);
+  }
+ } catch (error) {
+  console.error(
+   colors.red(`Error executing command ${cmd.join(" ")}:`),
+   error.message,
+  );
+  Deno.exit(1);
+ }
+}
+
+// Function to check if a file exists and delete it if it does
+async function checkAndDeleteFile(filePath: string) {
+ try {
+  const fileInfo = await Deno.stat(filePath);
+  if (fileInfo.isFile) {
+   console.log(colors.yellow(`File ${filePath} found. Deleting...`));
+   await Deno.remove(filePath);
+   console.log(colors.green(`File ${filePath} deleted.`));
+  }
+ } catch (error) {
+  if (error instanceof Deno.errors.NotFound) {
+   console.log(colors.green(`File ${filePath} not found. No action needed.`));
+  } else {
+   console.error(
+    colors.red(`Error checking or deleting file ${filePath}:`),
+    error.message,
+   );
+   Deno.exit(1);
+  }
+ }
+}
+
+// Check if a folder name was provided
+if (Deno.args.length === 0) {
+ console.error(
+  colors.red("No folder name provided. Please provide a folder name."),
+ );
+ Deno.exit(1);
+}
+
+// Store the folder name in a variable
+const folderName = Deno.args[0];
+
+// Path to the SQLite database file
+const dbFilePath = "./resource-surveillance.sqlite.db";
+
+// Define synchronous suppliers
+const deidentificationSQLSupplier: FlexibleTextSupplierSync = () =>
+ deidentificationSQL;
+const vvSQLSupplier: FlexibleTextSupplierSync = () => vvSQL;
+
+// Fetch and store UX SQL content
+let uxSQL: string;
+
+try {
+ const uxSQLContent = await drhux.drhSQL();
+ uxSQL = uxSQLContent.join("\n");
+} catch (error) {
+ console.error(colors.red("Error fetching UX SQL content:"), error.message);
+ Deno.exit(1);
+}
+
+// Define the SQL supplier for UX
+const uxSQLSupplier: FlexibleTextSupplierSync = () => uxSQL;
+
+let deidentificationSQL: string;
+let vvSQL: string;
+
+try {
+ // Fetch SQL content for DeIdentification and Verification & Validation
+ deidentificationSQL = await fetchSqlContent(
+  `${RSC_BASE_URL}/de-identification/drh-deidentification.sql`,
+ );
+ vvSQL = await fetchSqlContent(
+  `${RSC_BASE_URL}/verification-validation/orchestrate-drh-vv.sql`,
+ );
+} catch (error) {
+ console.error(
+  colors.red(
+   "Error fetching SQL contents for DeIdentification and Verification & Validation:",
+  ),
+  error.message,
+ );
+ Deno.exit(1);
+}
+
+// Check and delete the file if it exists
+await checkAndDeleteFile(dbFilePath);
+
+// Log the start of the process
+console.log(colors.cyan(`Starting the process for folder: ${folderName}`));
+
+try {
+ // Ingest files and orchestrate transform-csv
+ console.log(colors.dim(`Ingesting files from folder: ${folderName}...`));
+ await executeCommand([toolCmd, "ingest", "files", "-r", `${folderName}/`]);
+ await executeCommand([toolCmd, "orchestrate", "transform-csv"]);
+ console.log(
+  colors.green("Files ingestion and CSV transformation successful."),
+ );
+
+ // Perform DeIdentification
+ console.log(colors.dim(`Performing DeIdentification: ${folderName}...`));
+ await executeCommand(
+  [toolCmd, "orchestrate", "-n", "deidentification"],
+  deidentificationSQLSupplier,
+ );
+ console.log(colors.green("Deidentification successful."));
+
+ // Perform Verification and Validation
+ console.log(
+  colors.dim(`Performing Verification and Validation: ${folderName}...`),
+ );
+ await executeCommand([toolCmd, "orchestrate", "-n", "v&v"], vvSQLSupplier);
+ console.log(
+  colors.green(
+   "Verification and validation orchestration completed successfully.",
+  ),
+ );
+
+ // Perform UX orchestration
+ console.log(colors.dim(`Performing UX orchestration: ${folderName}...`));
+ await executeCommand([toolCmd, "orchestrate", "-n", "ux"], uxSQLSupplier);
+ console.log(colors.green("UX orchestration completed successfully."));
+
+ console.log(
+  colors.green(`Loading DRH Edge UI... at http://localhost:9000/drh/index.sql`),
+ );
+ await executeCommand([toolCmd, "web-ui", "--port", "9000"]);
+} catch (error) {
+ console.error(colors.red("An error occurred:"), error.message);
+ Deno.exit(1);
+}
